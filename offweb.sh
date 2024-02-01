246d9af86cb7b2a424769c6998fa9a27db3fdc15
Cavalo01 <124747668+Cavalo01@users.noreply.github.com>
2024-01-31 21:28:31 -0300

    offweb.sh

--- offweb.sh
+++ offweb.sh
@@ -1,4 +1,11 @@
-#!/bin/bash                                                                                                 wget -p -k "$@" >/dev/null 2>&1                       export init=$(echo $((RANDOM%(65539-1024+1)+1024))) 
-cd "$@"                                               echo "Este é o link da sua página offline: 0.0.0.0:$init"                                                   python -m http.server $init >/dev/null 2>&1           
-echo "Quando quiser rodar a página offline novamente, Volte em `pwd` e execute offwebinit"                  
-echo "echo Este é o link da sua página offline: 0.0.0.0:$init                                               python -m http.server $init >/dev/null 2>&1" > /bin/offwebinit                                              chmod +x /bin/offwebinit
+#!/bin/bash
+
+wget -p -k "$@" >/dev/null 2>&1
+export init=$(echo $((RANDOM%(65539-1024+1)+1024)))
+cd "$@"
+echo "Este é o link da sua página offline: 0.0.0.0:$init"
+python -m http.server $init >/dev/null 2>&1
+echo "Quando quiser rodar a página offline novamente, Volte em `pwd` e execute offwebinit"
+echo "echo Este é o link da sua página offline: 0.0.0.0:$init
+python -m http.server $init >/dev/null 2>&1" > /bin/offwebinit
+chmod +x /bin/offwebinit
